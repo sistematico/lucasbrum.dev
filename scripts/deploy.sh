8fed07ad67237c5c8f516c1ac2e312911fbb2f28
Lucas Saliés Brum <sistematico@gmail.com>
2025-05-12 06:56:16 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,13 @@ PATH=$PATH:/home/nginx/.bun/bin
 [ -f .env.production ] && cp .env.production /tmp/env.lucasbrum
 git clean -fxd
 [ -f /tmp/env.lucasbrum ] && cp /tmp/env.lucasbrum .env.production
-sudo /usr/bin/systemctl stop lucasbrum.service
+#sudo /usr/bin/systemctl stop lucasbrum.service
 bun install
-bun run db:drop
-bun run db:generate
-bun run db:push
-bun run db:migrate
-bun run db:seed
+bun run build
+#bun run db:drop
+#bun run db:generate
+#bun run db:push
+#bun run db:migrate
+#bun run db:seed
 #bun run build
-sudo /usr/bin/systemctl start privatehub.service
+#sudo /usr/bin/systemctl start privatehub.service
