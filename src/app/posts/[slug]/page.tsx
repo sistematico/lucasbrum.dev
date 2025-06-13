8b532ff74d30b54a8f1c6a0e387fecd472ed8307
Lucas Saliés Brum <sistematico@gmail.com>
2025-06-13 03:41:40 -0400

    Update

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -14,7 +14,9 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({
   params,
-}: { params: { slug: string } }): Promise<Metadata | undefined> {
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata | undefined> {
   const { slug } = await params;
   let post = getBlogPosts().find((post) => post.slug === slug);
   if (!post) return;
@@ -54,7 +56,7 @@ export async function generateMetadata({
 }
 
 export default async function Blog({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+  const { slug } = params;
   let post = getBlogPosts().find((post) => post.slug === slug);
 
   if (!post) {
