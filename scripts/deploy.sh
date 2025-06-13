53a333bedaf5d910a4c01a436b6f7b452686d74f
Lucas Saliés Brum <sistematico@gmail.com>
2025-06-13 02:08:36 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@ PATH=$PATH:/home/nginx/.bun/bin
 
 sudo /usr/bin/systemctl stop blog.lucasbrum.dev.service
 
-[ -e .env.production ] && cp .env.production /tmp/.env.production.blog
+[ -e .env.production ] && cp -f .env.production /tmp/.env.production.blog
 
-git clean -fxd
+#git clean -fxd
 
-[ -e /tmp/.env.production.blog ] && cp /tmp/.env.production.blog .env.production
+[ -e /tmp/.env.production.blog ] && cp -f /tmp/.env.production.blog .env.production
 
 bun install
 bun run build
