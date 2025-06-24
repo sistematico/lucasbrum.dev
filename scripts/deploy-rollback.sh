a22273f45cda762d8d9ce2aff2adcd7357de3995
Lucas Saliés Brum <sistematico@gmail.com>
2025-06-24 11:57:45 -0400

    Update

--- scripts/deploy-rollback.sh
+++ scripts/deploy-rollback.sh
@@ -21,7 +21,7 @@ mkdir -p $BACKUP_DIR
 
 # Backup da versão atual funcionando
 echo "Fazendo backup da versão atual..."
-sudo systemctl is-active $SERVICE && {
+sudo /usr/bin/systemctl is-active $SERVICE && {
     cd $CURRENT_DIR
     mkdir -p $BACKUP_DIR/$DEPLOY_TIMESTAMP
     cp -a .next node_modules package.json bun.lockb .env.production $BACKUP_DIR/$DEPLOY_TIMESTAMP/
