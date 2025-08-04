625868375e6c2d8be1a9d59f47be5671329b4ca0
Lucas Saliés Brum <sistematico@gmail.com>
2025-08-04 10:21:55 -0400

    Updated projects page

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -13,54 +13,36 @@ export default function ProjectCard({ title = "", description = "", link = "#", 
       <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
         {description}
       </p>
-      {/* <Link
+      <Link
+        className={`
+          inline-flex rounded-sm items-center gap-2 px-4 py-2 shadow-sm
+          border border-black/40 border-black/30 
+          text-black dark:text-white
+          hover:bg-transparent hover:text-black/70 dark:hover:text-white/70
+          focus:ring-3 focus:outline-hidden
+          transition-all duration-200 me-2
+        `}
         href={link}
-        className="inline-flex font-medium items-center text-blue-600 hover:underline me-2"
+        target="_blank"
       >
-        Demonstração
-        <svg
-          className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 18 18"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
-          />
-        </svg>
-      </Link> */}
-      {/* <Link
+        <Globe className="w-4 h-4" />
+        <span className="text-sm font-medium">Site</span>
+      </Link>
+      <Link
+        className={`
+          inline-flex rounded-sm items-center gap-2 px-4 py-2 shadow-sm
+          border border-black/40 border-black/30 
+          text-black dark:text-white
+          hover:bg-transparent hover:text-black/70 dark:hover:text-white/70
+          focus:ring-3 focus:outline-hidden
+          transition-all duration-200 me-2
+        `}
         href={repo}
-        className="inline-flex font-medium items-center text-blue-600 hover:underline"
+        target="_blank"
       >
-        Repositório
-        <svg
-          className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 18 18"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
-          />
-        </svg>
-      </Link> */}
-      <Button href={repo} icon={<Globe className="w-4 h-4" />} className="me-2">
-        Site
-      </Button>
-      <Button href={repo} icon={<GitBranch className="w-4 h-4" />}>
-        Repositório
-      </Button>
+        <GitBranch className="w-4 h-4" />
+        <span className="text-sm font-medium">Repositório</span>
+      </Link>
     </div>
   );
 }
